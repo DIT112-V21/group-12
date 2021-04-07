fef9be7d93a9f184b7bf9640c9bcac4dfa5f5cab
Amanda <amandaturquis@live.com>
2021-04-07 14:08:11 +0200

    Add basic steering functions  move forward and backward, turn left and right

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -1,9 +1,57 @@
 #include <Smartcar.h>
+ 
+// derived from [https://platisd.github.io/smartcar_shield/manual_control_8ino-example.html]
 
-void setup() {
-  // put your setup code here, to run once:
+
+const int fSpeed   = 70;  // 70% of the full speed forward
+const int bSpeed   = -70; // 70% of the full speed backward
+const int lDegrees = -75; // degrees to turn left
+const int rDegrees = 75;  // degrees to turn right
+ 
+ArduinoRuntime arduinoRuntime;
+BrushedMotor leftMotor(arduinoRuntime, smartcarlib::pins::v2::leftMotorPins);
+BrushedMotor rightMotor(arduinoRuntime, smartcarlib::pins::v2::rightMotorPins);
+DifferentialControl control(leftMotor, rightMotor);
+ 
+SimpleCar car(control);
+ 
+void setup()
+{
+    Serial.begin(9600);
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
+ 
+void loop()
+{
+    handleInput();
+}
+ 
+void handleInput()
+{ // handle serial input if there is any
+    if (Serial.available())
+    {
+        char input = Serial.read(); // read everything that has been received so far and log down
+                                    // the last entry
+        switch (input)
+        {
+        case 'l': // rotate counter-clockwise going forward
+            car.setSpeed(fSpeed);
+            car.setAngle(lDegrees);
+            break;
+        case 'r': // turn clock-wise
+            car.setSpeed(fSpeed);
+            car.setAngle(rDegrees);
+            break;
+        case 'f': // go ahead
+            car.setSpeed(fSpeed);
+            car.setAngle(0);
+            break;
+        case 'b': // go back
+            car.setSpeed(bSpeed);
+            car.setAngle(0);
+            break;
+        default: // if you receive something that you don't know, just stop
+            car.setSpeed(0);
+            car.setAngle(0);
+        }
+    }
 }
