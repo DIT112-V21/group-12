0bf4ef13042555d0b65895915ed4ad74f7a3d32c
maltegotharsson <gusgothama@student.gu.se>
2021-04-08 11:53:11 +0200

    Change velocity of car  4 different speed modes going backwards and forwards.  Developers; Malte, Maja, Bardha and Karl.

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -1,12 +1,14 @@
 #include <Smartcar.h>
  
-// derived from [https://platisd.github.io/smartcar_shield/manual_control_8ino-example.html]
+/*
+The skeleton for this code is derived from [https://platisd.github.io/smartcar_shield/manual_control_8ino-example.html]
+*/
 
-
-const int fSpeed   = 70;  // 70% of the full speed forward
-const int bSpeed   = -70; // 70% of the full speed backward
-const int lDegrees = -75; // degrees to turn left
-const int rDegrees = 75;  // degrees to turn right
+int fSpeed = 0;
+int bSpeed = 0; 
+int lDegrees = -75; // degrees to turn left
+int rDegrees = 75;  // degrees to turn right
+String direct;
  
 ArduinoRuntime arduinoRuntime;
 BrushedMotor leftMotor(arduinoRuntime, smartcarlib::pins::v2::leftMotorPins);
@@ -22,15 +24,19 @@ void setup()
  
 void loop()
 {
+    
     handleInput();
+   
 }
- 
-void handleInput()
-{ // handle serial input if there is any
-    if (Serial.available())
-    {
-        char input = Serial.read(); // read everything that has been received so far and log down
-                                    // the last entry
+
+/* 
+When standing still you need to choose a direction and then enter a speed mode 1-4.
+Invalid input will still get passed to setCarSpeed which will cause the car to stop.
+*/
+
+void handleInput(){
+  if (Serial.available()){
+    char input = Serial.read();
         switch (input)
         {
         case 'l': // rotate counter-clockwise going forward
@@ -44,14 +50,90 @@ void handleInput()
         case 'f': // go ahead
             car.setSpeed(fSpeed);
             car.setAngle(0);
+            direct = "f";
             break;
         case 'b': // go back
             car.setSpeed(bSpeed);
             car.setAngle(0);
-            break;
-        default: // if you receive something that you don't know, just stop
-            car.setSpeed(0);
-            car.setAngle(0);
-        }
+            direct = "b";
+            break;       
+        default:  
+             setCarSpeed(input, direct);
+            }
+        
     }
-}
+  }
+
+/*
+The method setCarSpeed takes the direction and the speed mode as arguments.
+For an invalid speed mode the car will stop.
+*/
+
+void setCarSpeed(char input, String direct){
+  switch(input){
+          case '1':
+          if (direct == "f"){
+            fSpeed = 10;
+            bSpeed = -10;
+            car.setSpeed(fSpeed);
+          }
+          else if(direct == "b"){
+            bSpeed = -10;
+            fSpeed = 10;
+            car.setSpeed(bSpeed);
+          }
+          else{
+            car.setSpeed(0);
+          }
+          
+          break;
+        case '2':
+        if (direct == "f"){
+            fSpeed = 40;
+            bSpeed = -40;
+            car.setSpeed(fSpeed);
+          }
+          else if(direct == "b"){
+            bSpeed = -40;
+            fSpeed = 40;
+            car.setSpeed(bSpeed);
+          }
+          else{
+            car.setSpeed(0);
+          }
+          break;
+        case '3':
+        if (direct == "f"){
+            fSpeed = 60;
+            bSpeed = -60;
+            car.setSpeed(fSpeed);
+          }
+          else if(direct == "b"){
+            bSpeed = -60;
+            fSpeed = 60;
+            car.setSpeed(bSpeed);
+          }
+          else{
+            car.setSpeed(0);
+          }
+          break;
+        case '4':
+        if (direct == "f"){
+            fSpeed = 90;
+            bSpeed = -90;
+            car.setSpeed(fSpeed);
+          }
+          else if(direct == "b"){
+            bSpeed = -90;
+            fSpeed = 90;
+            car.setSpeed(bSpeed);
+          }
+          else{
+            car.setSpeed(0);
+          }
+          break;
+          default: // if you receive something that you don't know, just stop
+          car.setSpeed(0);
+          car.setAngle(0);
+       }
+  }
