74566bc21a8ef90b752286ae29ced861aa0e5fb9
Amanda <amandaturquis@live.com>
2021-04-08 13:33:03 +0200

    Add basic avoidance obstacles

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -1,5 +1,6 @@
 #include <Smartcar.h>
- 
+
+
 // derived from [https://platisd.github.io/smartcar_shield/manual_control_8ino-example.html]
 
 
@@ -7,26 +8,48 @@ const int fSpeed   = 70;  // 70% of the full speed forward
 const int bSpeed   = -70; // 70% of the full speed backward
 const int lDegrees = -75; // degrees to turn left
 const int rDegrees = 75;  // degrees to turn right
- 
+
+const int TRIGGER_PIN           = 6; // D6
+const int ECHO_PIN              = 7; // D7
+const unsigned int MAX_DISTANCE = 100;
+
 ArduinoRuntime arduinoRuntime;
 BrushedMotor leftMotor(arduinoRuntime, smartcarlib::pins::v2::leftMotorPins);
 BrushedMotor rightMotor(arduinoRuntime, smartcarlib::pins::v2::rightMotorPins);
 DifferentialControl control(leftMotor, rightMotor);
- 
+
 SimpleCar car(control);
- 
+
+SR04 front(arduinoRuntime, TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+
 void setup()
 {
     Serial.begin(9600);
+    Serial.setTimeout(200);
+    
 }
- 
+
 void loop()
 {
     handleInput();
+    handleObstacle();
 }
- 
+
+void handleObstacle()
+{
+    int distance = front.getDistance();
+    if (distance != 0 && distance < 100)
+    { 
+        car.setSpeed(0);
+        
+    } else {
+        handleInput();
+    }
+        delay(100);
+}
+
 void handleInput()
-{ // handle serial input if there is any
+{
     if (Serial.available())
     {
         char input = Serial.read(); // read everything that has been received so far and log down
