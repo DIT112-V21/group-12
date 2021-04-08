93d16a671b47e1f52d5f5676fb9ba729e9ae82a5
majlind <majlind@student.chalmers.se>
2021-04-08 15:26:25 +0200

    Add manual breaking
    Four different breaking speed modes.
    
    Developers: Bardha, Malte, Maja.

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -1,36 +1,68 @@
 #include <Smartcar.h>
- 
-// derived from [https://platisd.github.io/smartcar_shield/manual_control_8ino-example.html]
 
 
-const int fSpeed   = 70;  // 70% of the full speed forward
-const int bSpeed   = -70; // 70% of the full speed backward
-const int lDegrees = -75; // degrees to turn left
-const int rDegrees = 75;  // degrees to turn right
+/*
+The skeleton for this code is derived from [https://platisd.github.io/smartcar_shield/manual_control_8ino-example.html]
+*/
+
+
+int fSpeed = 0;
+int bSpeed = 0; 
+int lDegrees = -75; // degrees to turn left
+int rDegrees = 75;  // degrees to turn right
+String direct;
+
+const int TRIGGER_PIN           = 6; // D6
+const int ECHO_PIN              = 7; // D7
+const unsigned int MAX_DISTANCE = 100;
+
  
 ArduinoRuntime arduinoRuntime;
 BrushedMotor leftMotor(arduinoRuntime, smartcarlib::pins::v2::leftMotorPins);
 BrushedMotor rightMotor(arduinoRuntime, smartcarlib::pins::v2::rightMotorPins);
 DifferentialControl control(leftMotor, rightMotor);
- 
+
 SimpleCar car(control);
- 
+
+SR04 front(arduinoRuntime, TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+
 void setup()
 {
     Serial.begin(9600);
+    Serial.setTimeout(200);
+    
 }
- 
+
 void loop()
 {
+    
     handleInput();
+    //handleObstacle();
 }
- 
-void handleInput()
-{ // handle serial input if there is any
-    if (Serial.available())
-    {
-        char input = Serial.read(); // read everything that has been received so far and log down
-                                    // the last entry
+
+void handleObstacle()
+{
+    int distance = front.getDistance();
+    if (distance != 0 && distance < 100)
+    { 
+        car.setSpeed(0);
+        
+    } else {
+        handleInput();
+    }
+        delay(100);
+}
+
+
+/* 
+When standing still you need to choose a direction and then enter a speed mode 1-4.
+Invalid input will still get passed to setCarSpeed which will cause the car to stop.
+*/
+
+void handleInput(){
+  if (Serial.available()){
+    char input = Serial.read();
+    String value;
         switch (input)
         {
         case 'l': // rotate counter-clockwise going forward
@@ -44,14 +76,127 @@ void handleInput()
         case 'f': // go ahead
             car.setSpeed(fSpeed);
             car.setAngle(0);
+            direct = "f";
             break;
         case 'b': // go back
             car.setSpeed(bSpeed);
             car.setAngle(0);
+            direct = "b";
             break;
-        default: // if you receive something that you don't know, just stop
+        case 'g': // go back
+            car.setSpeed(fSpeed * 0.9);
+            delay(700);
+            car.setSpeed(fSpeed * 0.7);
+            delay(700);
+            car.setSpeed(fSpeed * 0.5);
+            delay(700);
+            car.setSpeed(fSpeed * 0.3);
+            delay(700);
             car.setSpeed(0);
-            car.setAngle(0);
-        }
+            break; 
+        case 'h': // go back
+            car.setSpeed(fSpeed * 0.9);
+            delay(500);
+            car.setSpeed(fSpeed * 0.7);
+            delay(500);
+            car.setSpeed(fSpeed * 0.5);
+            delay(500);
+            car.setSpeed(fSpeed * 0.3);
+            delay(500);
+            car.setSpeed(0);
+            break;
+         case 'j': // go back
+            car.setSpeed(fSpeed * 0.9);
+            delay(200);
+            car.setSpeed(fSpeed * 0.7);
+            delay(200);
+            car.setSpeed(fSpeed * 0.5);
+            delay(200);
+            car.setSpeed(fSpeed * 0.3);
+            delay(200);
+            car.setSpeed(0);
+            break; 
+         case 'k': // go back
+            car.setSpeed(0);
+            break;       
+        default:  
+             setCarSpeed(input, direct);
+            }
+        
     }
-}
+  }
+ 
+
+/*
+The method setCarSpeed takes the direction and the speed mode as arguments.
+For an invalid speed mode the car will stop.
+*/
+
+void setCarSpeed(char input, String direct){
+  switch(input){
+          case '1':
+          if (direct == "f"){
+            fSpeed = 10;
+            bSpeed = -10;
+            car.setSpeed(fSpeed);
+          }
+          else if(direct == "b"){
+            bSpeed = -10;
+            fSpeed = 10;
+            car.setSpeed(bSpeed);
+          }
+          else{
+            car.setSpeed(0);
+          }
+          
+          break;
+        case '2':
+        if (direct == "f"){
+            fSpeed = 40;
+            bSpeed = -40;
+            car.setSpeed(fSpeed);
+          }
+          else if(direct == "b"){
+            bSpeed = -40;
+            fSpeed = 40;
+            car.setSpeed(bSpeed);
+          }
+          else{
+            car.setSpeed(0);
+          }
+          break;
+        case '3':
+        if (direct == "f"){
+            fSpeed = 60;
+            bSpeed = -60;
+            car.setSpeed(fSpeed);
+          }
+          else if(direct == "b"){
+            bSpeed = -60;
+            fSpeed = 60;
+            car.setSpeed(bSpeed);
+          }
+          else{
+            car.setSpeed(0);
+          }
+          break;
+        case '4':
+        if (direct == "f"){
+            fSpeed = 90;
+            bSpeed = -90;
+            car.setSpeed(fSpeed);
+          }
+          else if(direct == "b"){
+            bSpeed = -90;
+            fSpeed = 90;
+            car.setSpeed(bSpeed);
+          }
+          else{
+            car.setSpeed(0);
+          }
+          break;
+          default: // if you receive something that you don't know, just stop
+          car.setSpeed(0);
+          car.setAngle(0);
+       }
+  }
