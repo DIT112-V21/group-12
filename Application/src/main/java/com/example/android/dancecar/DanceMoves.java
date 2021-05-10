4722d1c3af6c2ec4d3308852b37e68e1ebd1760f
navyap <navya@student.chalmers.se>
2021-05-10 11:30:03 +0200

    Co-authored-by: Maltegos <gusgothama@student.gu.se> Add Dance Menu options

---
+++ Application/src/main/java/com/example/android/dancecar/DanceMoves.java
@@ -0,0 +1,153 @@
+package com.example.android.dancecar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class DanceMoves extends AppCompatActivity {
+    ArrayList danceMoves = new ArrayList();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dance_moves);
+    }
+
+    public void goToDrive(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void danceMove1(View view){
+         final CheckBox danceMove1 = findViewById(R.id.danceMove1);
+        if(danceMove1.isChecked()){
+            danceMoves.add("danceMove1");
+            TextView current = findViewById(R.id.currentDances);
+            current.setText(danceMoves.toString());
+        }else{
+            danceMoves.remove("danceMove1");
+            TextView current = findViewById(R.id.currentDances);
+            current.setText(danceMoves.toString());
+
+        }
+    }
+
+    public void danceMove2(View view){
+        final CheckBox danceMove2 = findViewById(R.id.danceMove2);
+        if(danceMove2.isChecked()){
+            danceMoves.add("danceMove2");
+            TextView current = findViewById(R.id.currentDances);
+            current.setText(danceMoves.toString());
+        }else{
+            danceMoves.remove("danceMove2");
+            TextView current = findViewById(R.id.currentDances);
+            current.setText(danceMoves.toString());
+        }
+    }
+
+    public void danceMove3(View view){
+        final CheckBox danceMove3 = findViewById(R.id.danceMove3);
+        if(danceMove3.isChecked()){
+            danceMoves.add("danceMove3");
+            TextView current = findViewById(R.id.currentDances);
+            current.setText(danceMoves.toString());
+        }else{
+            danceMoves.remove("danceMove3");
+            TextView current = findViewById(R.id.currentDances);
+            current.setText(danceMoves.toString());
+        }
+    }
+
+
+    public void danceMove4(View view){
+        final CheckBox danceMove4 = findViewById(R.id.danceMove4);
+        if(danceMove4.isChecked()){
+            danceMoves.add("danceMove4");
+            TextView current = findViewById(R.id.currentDances);
+            current.setText(danceMoves.toString());
+        }else{
+            danceMoves.remove("danceMove4");
+            TextView current = findViewById(R.id.currentDances);
+            current.setText(danceMoves.toString());
+        }
+    }
+
+    public void danceMove5(View view){
+        final CheckBox danceMove5 = findViewById(R.id.danceMove4);
+        if(danceMove5.isChecked()){
+            danceMoves.add("danceMove5");
+            TextView current = findViewById(R.id.currentDances);
+            current.setText(danceMoves.toString());
+        }else{
+            danceMoves.remove("danceMove5");
+            TextView current = findViewById(R.id.currentDances);
+            current.setText(danceMoves.toString());
+        }
+    }
+
+    public void danceMove6(View view){
+        final CheckBox danceMove6 = findViewById(R.id.danceMove4);
+        if(danceMove6.isChecked()){
+            danceMoves.add("danceMove6");
+            TextView current = findViewById(R.id.currentDances);
+            current.setText(danceMoves.toString());
+        }else{
+            danceMoves.remove("danceMove6");
+            TextView current = findViewById(R.id.currentDances);
+            current.setText(danceMoves.toString());
+        }
+    }
+
+    public void danceMove7(View view){
+        final CheckBox danceMove7 = findViewById(R.id.danceMove4);
+        if(danceMove7.isChecked()){
+            danceMoves.add("danceMove7");
+            TextView current = findViewById(R.id.currentDances);
+            current.setText(danceMoves.toString());
+        }else{
+            danceMoves.remove("danceMove7");
+            TextView current = findViewById(R.id.currentDances);
+            current.setText(danceMoves.toString());
+        }
+    }
+
+    public void danceMove8(View view){
+        final CheckBox danceMove8 = findViewById(R.id.danceMove4);
+        if(danceMove8.isChecked()){
+            danceMoves.add("danceMove8");
+            TextView current = findViewById(R.id.currentDances);
+            current.setText(danceMoves.toString());
+        }else{
+            danceMoves.remove("danceMove8");
+            TextView current = findViewById(R.id.currentDances);
+            current.setText(danceMoves.toString());
+        }
+    }
+    public void danceMove9(View view){
+        final CheckBox danceMove9 = findViewById(R.id.danceMove4);
+        if(danceMove9.isChecked()){
+            danceMoves.add("danceMove9");
+            TextView current = findViewById(R.id.currentDances);
+            current.setText(danceMoves.toString());
+        }else{
+            danceMoves.remove("danceMove9");
+            TextView current = findViewById(R.id.currentDances);
+            current.setText(danceMoves.toString());
+        }
+    }
+    public void createChoreography(View view){
+        //Add to Choreography list later
+
+    }
+
+
+}
--- Application/src/main/java/com/example/android/dancecar/MainActivity.java
+++ Application/src/main/java/com/example/android/dancecar/MainActivity.java
@@ -2,14 +2,9 @@ package com.example.android.dancecar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.AlertDialog;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
@@ -24,88 +19,12 @@ public class MainActivity extends AppCompatActivity {
     private MqttClient mMqttClient;
     private boolean isConnected = false;
 
-    EditText move_name, direction;
-    Button insert, update, delete, view;
-    DBHelper DB;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mMqttClient = new MqttClient(getApplicationContext(), MQTT_SERVER, TAG);
         connectToMqttBroker();
-
-        move_name = findViewById(R.id.move_name);
-        direction = findViewById(R.id.direction);
-
-        insert = findViewById(R.id.btnInsert);
-        update = findViewById(R.id.btnUpdate);
-        delete = findViewById(R.id.btnDelete);
-        view = findViewById(R.id.btnView);
-        DB = new DBHelper(this);
-
-        insert.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String move_nameTXT = move_name.getText().toString();
-                String directionTXT = direction.getText().toString();
-
-                Boolean checkInsertData = DB.insertMoveData(move_nameTXT, directionTXT);
-                if(checkInsertData==true)
-                    Toast.makeText(MainActivity.this, "New move inserted!", Toast.LENGTH_SHORT).show();
-                else
-                    Toast.makeText(MainActivity.this, "New move not inserted!", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        update.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String move_nameTXT = move_name.getText().toString();
-                String directionTXT = direction.getText().toString();
-
-                Boolean checkUpdateData = DB.updateMoveData(move_nameTXT, directionTXT);
-                if(checkUpdateData==true)
-                    Toast.makeText(MainActivity.this, "Move updated!", Toast.LENGTH_SHORT).show();
-                else
-                    Toast.makeText(MainActivity.this, "Move not updated!", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        delete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String move_nameTXT = move_name.getText().toString();
-
-                Boolean checkDeleteData = DB.deleteData(move_nameTXT);
-                if(checkDeleteData==true)
-                    Toast.makeText(MainActivity.this, "Move deleted!", Toast.LENGTH_SHORT).show();
-                else
-                    Toast.makeText(MainActivity.this, "Move not deleted!", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Cursor res = DB.getData();
-                if(res.getCount()==0){
-                    Toast.makeText(MainActivity.this, "No move found!", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                StringBuffer buffer = new StringBuffer();
-                while(res.moveToNext()){
-                    buffer.append("Move name :"+res.getString(0)+"\n");
-                    buffer.append("Direction :"+res.getString(1)+"\n\n");
-                }
-
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setCancelable(true);
-                builder.setTitle("New move!");
-                builder.setMessage(buffer.toString());
-                builder.show();
-            }
-        });
     }
 
     @Override
