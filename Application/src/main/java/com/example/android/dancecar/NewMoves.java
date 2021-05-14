8869b71168ad7e643b691bd81997dd08831ffec1
Meis <79854458+meissalim@users.noreply.github.com>
2021-05-14 10:02:06 +0200

    #31 Remove mode/number buttons that are not used Co-authored-by: Maja Linder <guskalmas@student.gu.se>, Karl Stahre <karlstahre@gmail.com>

--- Application/src/main/java/com/example/android/dancecar/NewMoves.java
+++ Application/src/main/java/com/example/android/dancecar/NewMoves.java
@@ -34,30 +34,16 @@ public class NewMoves extends AppCompatActivity {
     private String currentSpeed;
 
     private CountDownTimer countDownTimer;
-    private long timeLeft = 10000;
+    private long timeLeft = 30000;
     private boolean isRecording = false;
     private String timerText = "";
     private long duration;
-
-    private Mode speedMode = new Mode("speed");
-    private Mode angleMode = new Mode("angle");
-    private Mode brakeMode = new Mode("brake");
 
     private ImageButton forward;
     private ImageButton backward;
     private ImageButton left;
     private ImageButton right;
 
-    private Button speed;
-    private Button angle;
-    private Button brake;
-    private Button one;
-    private Button two;
-    private Button three;
-    private Button four;
-    private Button currentSpeedMode;
-    private Button currentAngleMode;
-    private Button currentBrakeMode;
     private Button speedometer;
 
     private TextView recordingTimer;
@@ -203,16 +189,6 @@ public class NewMoves extends AppCompatActivity {
         backward = findViewById(R.id.arrowDown);
         left = findViewById(R.id.arrowLeft);
         right = findViewById(R.id.arrowRight);
-        speed = findViewById(R.id.speed);
-        angle = findViewById(R.id.angle);
-        brake = findViewById(R.id.brake);
-        one = findViewById(R.id.button1);
-        two = findViewById(R.id.button2);
-        three = findViewById(R.id.button3);
-        four = findViewById(R.id.button4);
-        currentSpeedMode = findViewById(R.id.currentSpeedMode);
-        currentAngleMode = findViewById(R.id.currentAngleMode);
-        currentBrakeMode = findViewById(R.id.currentBrakeMode);
         speedometer = findViewById(R.id.currentSpeed);
         recordingTimer = findViewById(R.id.recordingTimer);
         startStop = findViewById(R.id.startstopButton);
@@ -234,7 +210,7 @@ public class NewMoves extends AppCompatActivity {
         timeLeft = 10000;
         timerText = "";
         recordingTimer.setText(timerText);
-        uncolorButtons("arrow");
+        uncolorButtons();
         direction = "";
         lastDirection = "";
         stopWatch.stop();
@@ -367,7 +343,7 @@ public class NewMoves extends AppCompatActivity {
     The other buttons will get unpressed when one button is pressed.
     */
     public void colorArrowButtons(String direction){
-        uncolorButtons("arrow");
+        uncolorButtons();
         if (direction.equals("forward")) {
             colorImageButton(forward);
         } else if (direction.equals("backward")) {
@@ -379,171 +355,12 @@ public class NewMoves extends AppCompatActivity {
         }
     }
 
-    public void numberModeOne(View view){
-        setCurrentModeNumber(1);
-        colorNumberButtons("one");
-        updateAndShowModeNumbers();
-        int message = 0;
-        mMqttClient.publish("smartcar/speedOne", Integer.toString(message), 1, null);
-    }
-    public void numberModeTwo(View view){
-        setCurrentModeNumber(2);
-        colorNumberButtons("two");
-        updateAndShowModeNumbers();
-        int message = 0;
-        mMqttClient.publish("smartcar/speedTwo", Integer.toString(message), 1, null);
-    }
-    public void numberModeThree(View view){
-        setCurrentModeNumber(3);
-        colorNumberButtons("three");
-        updateAndShowModeNumbers();
-        int message = 0;
-        mMqttClient.publish("smartcar/speedThree", Integer.toString(message), 1, null);
-    }
-    public void numberModeFour(View view){
-        setCurrentModeNumber(4);
-        colorNumberButtons("four");
-        updateAndShowModeNumbers();
-        int message = 0;
-        mMqttClient.publish("smartcar/speedFour", Integer.toString(message), 1, null);
-    }
-
-    public void setCurrentModeNumber(int number) {
-        if (speedMode.isActivated()) {
-            speedMode.setNumber(number);
-        } else if (angleMode.isActivated()) {
-            angleMode.setNumber(number);
-        } else if (brakeMode.isActivated()) {
-            brakeMode.setNumber(number);
-        } else {
-            uncolorButtons("number");
-        }
-    }
-
-    /*
-    Only one number button can show as pressed/colored at a time.
-    The other buttons will get unpressed when one button is pressed.
-     */
-    public void colorNumberButtons(String number){
-        uncolorButtons("number");
-        if(number.equals("one")) {
-            colorButton(one);
-        } else if (number.equals("two")) {
-            colorButton(two);
-        } else if (number.equals("three")) {
-            colorButton(three);
-        } else if (number.equals("four")) {
-            colorButton(four);
-        }
-    }
-
-    /*
-    A number is shown underneath the three different mode buttons
-    to show in which number that mode is currently in.
-    */
-    public void updateAndShowModeNumbers(){
-        if (speedMode.isActivated()){
-            String number = Integer.toString(speedMode.getNumber());
-            currentSpeedMode.setText(number);
-        } else if (angleMode.isActivated()){
-            String number = Integer.toString(angleMode.getNumber());
-            currentAngleMode.setText(number);
-        } else if (brakeMode.isActivated()){
-            String number = Integer.toString(brakeMode.getNumber());
-            currentBrakeMode.setText(number);
-        } else {
-            currentSpeedMode.setText("");
-            currentAngleMode.setText("");
-            currentBrakeMode.setText("");
-        }
-    }
-
-    public void speedPress(View view){
-        speedMode.setActivated(true);
-        angleMode.setActivated(false);
-        brakeMode.setActivated(false);
-        int message = 0;
-        numberAlreadyActivated();
-        colorModeButtons();
-        mMqttClient.publish("smartcar/speedPress", Integer.toString(message), 1, null);
-    }
-
-    public void anglePress(View view){
-        angleMode.setActivated(true);
-        brakeMode.setActivated(false);
-        speedMode.setActivated(false);
-        int message = 0;
-        numberAlreadyActivated();
-        colorModeButtons();
-        mMqttClient.publish("smartcar/anglePress", Integer.toString(message), 1, null);
-    }
-
-    public void brakePress(View view){
-        brakeMode.setActivated(true);
-        speedMode.setActivated(false);
-        angleMode.setActivated(false);
-        int message = 0;
-        numberAlreadyActivated();
-        colorModeButtons();
-        mMqttClient.publish("smartcar/brakePress", Integer.toString(message), 1, null);
-    }
-
-    /*
-    When a mode has already been activated earlier,
-    when choosing that mode again, the current number
-    that mode is in shows as pressed/colored.
-    */
-    public void numberAlreadyActivated() {
-        int number = getCurrentModeNumber();
-        uncolorButtons("number");
-        if (number == 1) {
-            colorButton(one);
-        } else if (number == 2) {
-            colorButton(two);
-        } else if (number == 3) {
-            colorButton(three);
-        } else if (number == 4) {
-            colorButton(four);
-        }
-    }
-
-    public int getCurrentModeNumber() {
-        if (speedMode.isActivated()) {
-            return speedMode.getNumber();
-        } else if (angleMode.isActivated()) {
-            return angleMode.getNumber();
-        } else if (brakeMode.isActivated()) {
-            return brakeMode.getNumber();
-        } else {
-            return 0;
-        }
-    }
-
-    public void colorModeButtons(){
-        uncolorButtons("mode");
-        if (speedMode.isActivated()) {
-            colorButton(speed);
-        } else if (angleMode.isActivated()) {
-            colorButton(angle);
-        } else if (brakeMode.isActivated()) {
-            colorButton(brake);
-        }
-    }
-
     public void showSpeed(String message){
         speedometer.setText(message);
     }
 
     public void resetSettings(){
-        speedMode.setActivated(false);
-        angleMode.setActivated(false);
-        brakeMode.setActivated(false);
-        speedMode.setNumber(0);
-        angleMode.setNumber(0);
-        brakeMode.setNumber(0);
-        updateAndShowModeNumbers();
-        numberAlreadyActivated();
-        uncolorButtons("all");
+        uncolorButtons();
     }
 
     public void colorImageButton(ImageButton button){
@@ -554,33 +371,10 @@ public class NewMoves extends AppCompatActivity {
         button.setBackgroundColor(Color.parseColor("#ED2E3C34"));
     }
 
-    public void uncolorButtons(String type) {
-        if (type.equals("mode")) {
-            speed.setBackgroundColor(Color.parseColor("#8BC34A"));
-            angle.setBackgroundColor(Color.parseColor("#8BC34A"));
-            brake.setBackgroundColor(Color.parseColor("#8BC34A"));
-        } else if (type.equals("number")) {
-            one.setBackgroundColor(Color.parseColor("#8BC34A"));
-            two.setBackgroundColor(Color.parseColor("#8BC34A"));
-            three.setBackgroundColor(Color.parseColor("#8BC34A"));
-            four.setBackgroundColor(Color.parseColor("#8BC34A"));
-        } else if (type.equals("arrow")) {
-            forward.setColorFilter(Color.TRANSPARENT);
-            backward.setColorFilter(Color.TRANSPARENT);
-            left.setColorFilter(Color.TRANSPARENT);
-            right.setColorFilter(Color.TRANSPARENT);
-        } else {
-            speed.setBackgroundColor(Color.parseColor("#8BC34A"));
-            angle.setBackgroundColor(Color.parseColor("#8BC34A"));
-            brake.setBackgroundColor(Color.parseColor("#8BC34A"));
-            one.setBackgroundColor(Color.parseColor("#8BC34A"));
-            two.setBackgroundColor(Color.parseColor("#8BC34A"));
-            three.setBackgroundColor(Color.parseColor("#8BC34A"));
-            four.setBackgroundColor(Color.parseColor("#8BC34A"));
-            forward.setColorFilter(Color.TRANSPARENT);
-            backward.setColorFilter(Color.TRANSPARENT);
-            left.setColorFilter(Color.TRANSPARENT);
-            right.setColorFilter(Color.TRANSPARENT);
-        }
+    public void uncolorButtons() {
+        forward.setColorFilter(Color.TRANSPARENT);
+        backward.setColorFilter(Color.TRANSPARENT);
+        left.setColorFilter(Color.TRANSPARENT);
+        right.setColorFilter(Color.TRANSPARENT);
     }
 }
